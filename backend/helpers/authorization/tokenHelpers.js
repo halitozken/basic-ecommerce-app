4e5303c79fb3919b9841b1942c9d5a1f05620db9
Halit Özken <halitozken@gmail.com>
2022-11-10 13:33:06 +0300

    complete tokenHelpers functions

--- backend/helpers/authorization/tokenHelpers.js
+++ backend/helpers/authorization/tokenHelpers.js
@@ -26,3 +26,11 @@ const isTokenIncluded = (req) => {
     req.headers.authorization.startsWith("Bearer: ")
   );
 };
+
+const getAccessTokenFromHeader = (req) => {
+  const authorization = req.headers.authorization;
+  const access_token = authorization.split(" ")[1];
+  return access_token;
+};
+
+export { sendJwtToClient, isTokenIncluded, getAccessTokenFromHeader };
